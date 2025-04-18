357109038a3f76a94641026bc18fee15119743b1
Dhyan Shyam <dhyan.shyam@students.plymouth.ac.uk>
2025-04-18 23:50:54 +0100

    REVERT

--- Terminal_Portfolio/dist/MediaBoard/config.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://yvtiybyuifkiwyrnjebe.supabase.co";
-const SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2dGl5Ynl1aWZraXd5cm5qZWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxMDI2MDAsImV4cCI6MjA1MzY3ODYwMH0.HDfXGowPGlrthUD87x2uNsprx_xySFiGojFcgabLtxQ";
-const TMDB_API_KEY = "784d3314cee165d07cc55d1ac2f027ee";
-const TMDB_BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_API_KEY); 
